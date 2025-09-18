a26774356d88b3b4a406864c8416795dc7f08817
Ester <estelar@aluno.unifapce.edu.br>
2025-09-18 18:01:08 -0300

    Frontend Deashboard

--- frontend/src/pages/statistics/Statistics.tsx
+++ frontend/src/pages/statistics/Statistics.tsx
@@ -5,13 +5,15 @@ import Filters from "@/components/Filters";
 const Statistics = () => {
   return (
     <CryptoProvider>
-      <div><h1>Statistics</h1></div>
 
       <div className="w-[80%]  flex flex-col  mb-24 relative mx-auto">
         <Filters/>
         <CryptoTable />
       </div>
+
+  
     </CryptoProvider>
+    
   );
 };
 
